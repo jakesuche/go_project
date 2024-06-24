540cde74bae388d9258f98ac74a1505631bebc4f
jakeuche <uchechidi9@gmail.com>
2024-06-24 11:57:37 +0100

    update

---
+++ CARDS/deck.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// create a new type of deck
+// which is a slice of strings
+// adorenke
+
+
+type deck []string
+
+
+
+func newDeck() deck {
+	cards := deck{}
+	cardsSuites := []string {"Spades", "Diamonds", "Hearts", "Clubs", "Kings"}
+	cardValues := []string {"Ace", "Two", "Three", "Four", "Five"}
+	for _, suit := range cardsSuites{
+		for _, values := range cardValues {
+			cards = append(cards, suit+ " of " + values)
+		}
+	}
+
+	return cards
+	
+}
+
+func (d deck) print(){	
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int){
+
+}
